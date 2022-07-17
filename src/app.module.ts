2780164271f1d71e3389a1cdd229e7b05a340cfb
HyeokJoon Kong <orijoon98@gmail.com>
2022-07-18 00:55:16 +0900

    [feat/#7] 전체 카테고리 조회 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from 'src/config/common.module';
 import { ConfigurationModule } from 'src/config/config.module';
 import { AppController } from './app.controller';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { CategoriesModule } from './modules/categories/categories.module';
 import { PostsModule } from './modules/posts/posts.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [ConfigurationModule, PrismaModule, CommonModule, UsersModule, PostsModule],
+  imports: [ConfigurationModule, PrismaModule, CommonModule, UsersModule, PostsModule, CategoriesModule],
   controllers: [AppController],
   providers: [],
 })
