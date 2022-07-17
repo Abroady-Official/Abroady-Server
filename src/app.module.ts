78d112a8d3db6bc6ebdfb4151c65b3db3ef3be24
HyeokJoon Kong <orijoon98@gmail.com>
2022-07-17 21:01:58 +0900

    Revert "게시글 CRUD 추가"

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from 'src/config/common.module';
 import { ConfigurationModule } from 'src/config/config.module';
 import { AppController } from './app.controller';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
-import { PostsModule } from './modules/posts/posts.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [ConfigurationModule, PrismaModule, CommonModule, UsersModule, PostsModule],
+  imports: [ConfigurationModule, PrismaModule, CommonModule, UsersModule],
   controllers: [AppController],
   providers: [],
 })
