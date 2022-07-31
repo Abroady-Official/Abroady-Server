52617a8e448cdfdab9d027bec7e49601271f98e8
SEHOON <devkwonsehoon@naver.com>
2022-07-31 22:00:47 +0900

    [UPDATE] DTO 수정

--- src/modules/auth/dto/auth-post.req.dto.ts
+++ src/modules/auth/dto/auth-post.req.dto.ts
@@ -1,5 +1,4 @@
 import { PickType } from '@nestjs/swagger';
-import { IsIn, IsNotEmpty, IsString } from 'class-validator';
 import { UserDTO } from 'src/modules/users/dto/user.dto';
 
 export class AuthPostReqDTO extends PickType(UserDTO, ['social', 'uuid', 'nickname']) {}
